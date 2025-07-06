ec188e4f95f9e92c9a6aa9b97feba991692239bc
sauvik <sauvik@logic-square.com>
2025-07-06 22:35:48 +0530

    feat: updated like model for better funtioning

--- src/models/like.model.js
+++ src/models/like.model.js
@@ -2,21 +2,31 @@ import mongoose from "mongoose";
 
 const likeSchema = new mongoose.Schema(
   {
-    video: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Video",
-    },
-    comment: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Comment",
-    },
-    tweet: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Tweet",
-    },
+    // video: {
+    //   type: mongoose.Schema.Types.ObjectId,
+    //   ref: "Video",
+    // },
+    // comment: {
+    //   type: mongoose.Schema.Types.ObjectId,
+    //   ref: "Comment",
+    // },
+    // tweet: {
+    //   type: mongoose.Schema.Types.ObjectId,
+    //   ref: "Tweet",
+    // },
     likedBy: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
+      required: true,
+    },
+    likedOn: {
+      type: mongoose.Schema.Types.ObjectId,
+      refPath: "likedOnType",
+      required: true,
+    },
+    likedOnType: {
+      type: String,
+      enum: ["Tweet", "Comment", "Video"],
       required: true,
     },
   },
